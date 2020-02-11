f96cb766e3a8d9cf7d588f6a1c0e2934f8525782
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2020-02-11 10:25:21 -0500

    Hide traveling and fallen players

--- findReviveTargets.js
+++ findReviveTargets.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Okay Status
 // @namespace    torncat
-// @version      0.1
+// @version      0.1.1
 // @description  This script hides players with 'Okay' status to find revive targets faster.
 // @author       Wingmanjd[2127679]
 // @match        https://www.torn.com/*
@@ -13,9 +13,19 @@
     const D = document;
     const $$ = (selector, startNode = D) => [...startNode.querySelectorAll(selector)];
 
-    var statusList = $$('.status .t-green');
-    statusList.forEach(el => {
+    var greenStatusList = $$('.status .t-green');
+    var redStatusList = $$('.status .t-red');
+    greenStatusList.forEach(el => {
         var line = $(el).closest('li');
         $(line).hide();
     });
+
+    redStatusList.forEach(el => {
+        if ($(el).html() == 'Traveling' || $(el).html() == "Fallen") {
+            var line = $(el).closest('li');
+            $(line).hide();
+        }
+    });
+
+
 })();
